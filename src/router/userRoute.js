1ed25b9254dd74479c01d084faef6a25f54a8398
hembee <hembee999@gmail.com>
2023-07-31 19:48:13 +0100

    testt

--- src/router/userRoute.js
+++ src/router/userRoute.js
@@ -4,6 +4,9 @@ const tryCatchHandler = require("../utils/tryCatchHandler.js");
 
 const userRouter = express.Router();
 
+userRouter.get("/", () => {
+  res.send("Welcome to cravings meals, satisfy your cravings.");
+});
 userRouter.post(
   "/signup",
   tryCatchHandler(userController.userSignupController)
